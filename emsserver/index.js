609554b7230bab0b5e94e79e466eeb5825d62919
prxneeth <praneethreddyg1818@gmail.com>
2024-12-01 22:49:29 +0530

    passchange

--- emsserver/index.js
+++ emsserver/index.js
@@ -8,6 +8,7 @@ import departmentRouter from './routes/department.js'
 import employeeRouter from './routes/employee.js'
 import salaryRouter from './routes/salary.js'
 import leaveRouter from './routes/leave.js'
+import settingRouter from './routes/setting.js'
 
 
 connectToDatabase()
@@ -20,6 +21,7 @@ app.use('/api/department', departmentRouter)
 app.use('/api/employee', employeeRouter)
 app.use('/api/salary', salaryRouter)
 app.use('/api/leave', leaveRouter)
+app.use('/api/setting', settingRouter)
 
 
 
