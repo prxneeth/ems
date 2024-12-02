217935aed3ae0f798d18e6f02680ea6b35efd94f
prxneeth <praneethreddyg1818@gmail.com>
2024-12-02 20:22:37 +0530

    cascade delete,

--- emsserver/controllers/departmentController.js
+++ emsserver/controllers/departmentController.js
@@ -59,7 +59,8 @@ const deleteDepartment = async (req, res) => {
     try {
         const { id } = req.params
 
-        const deleteDep = await Department.findByIdAndDelete({ _id: id })
+        const deleteDep = await Department.findById({ _id: id })
+        await deleteDep.deleteOne()
         return res.status(200).json({ success: true, deleteDep })
     } catch (error) {
         return res.status(500).json({ success: false, error: "delete department server error" })
